faca48787c6bbcbb6ca16b64d03a761432f93340
sarfraj0304 <sarfrajahmadraza@gmail.com>
2023-04-06 14:33:20 +0530

    so

--- models/AppointmentModel.js
+++ models/AppointmentModel.js
@@ -3,11 +3,11 @@ const appointmentSchema = mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   specialization: { type: String, required: true },
-  experience: { type: Number, required: true },
+  experience: { type: String, required: true },
   location: { type: String, required: true },
-  date: { type: Date, required: true },
-  slots: { type: Number, required: true },
-  fee: { type: Number, required: true },
+  date: { type: String, required: true },
+  slots: { type: String, required: true },
+  fee: { type: String, required: true },
 });
 
 const appointmentModel = mongoose.model("appointment", appointmentSchema);
